e4d8abede721f1d88bb97f4bfd607991052315f6
ashenbandara02 <bandarashen02@gmail.com>
2025-09-11 17:00:21 +0530

    Project Initiated

---
+++ cakify/src/main/java/com/cakify/CakifyApplication.java
@@ -0,0 +1,13 @@
+package com.cakify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CakifyApplication.class, args);
+    }
+
+}
---
+++ cakify/src/test/java/com/cakify/CakifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

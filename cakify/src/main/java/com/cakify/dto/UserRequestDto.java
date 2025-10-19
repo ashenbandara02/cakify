29518d2b885f419dc81bafc70eaa006913c8415f
ashenbandara02 <bandarashen02@gmail.com>
2025-10-19 13:01:57 +0530

    User Request and User Reponse Dtos updated

---
+++ cakify/src/main/java/com/cakify/dto/UserRequestDto.java
@@ -0,0 +1,28 @@
+package com.cakify.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserRequestDto {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+
+    @Size(max = 50, message = "First name must not exceed 50 characters")
+    private String firstName;
+
+    @Size(max = 50, message = "Last name must not exceed 50 characters")
+    private String lastName;
+
+    @Builder.Default
+    private boolean enabled = true;
+}
